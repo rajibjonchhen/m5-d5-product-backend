6b3fd98cada5042b1649b458b37ecf90321ee4ef
yonatanski <phpcodeyg@gmail.com>
2022-01-28 16:15:59 +0100

    file upload is done &created public-folder again

--- src/service/lib/fs-tools.js
+++ src/service/lib/fs-tools.js
@@ -1,17 +1,32 @@
-import express from 'express'
-import { dirname,join, extname } from 'path'
-import { fileURLToPath } from 'url'
-import fs  from 'fs-extra'
+import express from "express"
+import { dirname, join, extname } from "path"
+import { fileURLToPath } from "url"
+import fs from "fs-extra"
 
-const {readJSON, writeJSON, writeFile} = fs
+const { readJSON, writeJSON, writeFile } = fs
 
-const datafolderPath  = join(dirname(fileURLToPath(import.meta.url)), '../data')
-const fullProductjsonPath = join(datafolderPath, 'products.json')
-const publicFolderPath = join (process.cwd(),'./public/image')
-
+const datafolderPath = join(dirname(fileURLToPath(import.meta.url)), "../data")
+const fullProductjsonPath = join(datafolderPath, "products.json")
+const publicFolderPath = join(process.cwd(), "./public/image")
 
 export const getProducts = () => readJSON(fullProductjsonPath)
 export const writeProducts = (content) => writeJSON(fullProductjsonPath, content)
 
-export const saveProductImg = (filename, buffer) => writeFile(join(publicFolderPath,filename),buffer)
-console.log ( datafolderPath)
+// export const saveProductImg = (filename, buffer) => writeFile(join(publicFolderPath, filename), buffer)
+console.log(datafolderPath)
+
+export const uploadFile = (req, res, next) => {
+  try {
+    const { originalname, buffer } = req.file
+    const extenstion = extname(originalname)
+
+    const fileName = `${req.params.id}${extenstion}`
+    const pathToFile = join(publicFolderPath, fileName)
+
+    fs.writeFileSync(pathToFile, buffer)
+    const imageUrl = `http://localhost:3001/${fileName} `
+    req.file = imageUrl
+  } catch (error) {
+    next(error)
+  }
+}
--- src/service/server/product/index.js
+++ src/service/server/product/index.js
@@ -1,180 +1,184 @@
 import express from "express"
 import multer from "multer"
-import uniqid from 'uniqid'
-import {saveProductImg, getProducts, writeProducts} from '../../lib/fs-tools.js'
+import uniqid from "uniqid"
+import { getProducts, writeProducts, uploadFile } from "../../lib/fs-tools.js"
 import { validationResult } from "express-validator"
-import createHttpError from 'http-errors'
+import createHttpError from "http-errors"
 import { productValidator } from "./productValidator.js"
 import { reviewValidator } from "./reviewValidator.js"
 
-
 const productsRouter = express.Router()
 
-
 // POST ************************ Add a new product*********************************************
-productsRouter.post('/',productValidator, async(req,res,next)=>{ 
-try {
+productsRouter.post("/", productValidator, async (req, res, next) => {
+  try {
     const errors = validationResult(req)
 
-   if(errors.isEmpty()){
-    const productsArray = await getProducts() 
-    const newProduct = {...req.body, _id:uniqid(), createAt: new Date(), imageUrl:`http://localhost:300/`}   
-    productsArray.push(newProduct)
-    await writeProducts(productsArray)
-    res.status(201).send(`New product added with id - ${newProduct._id}`)
-   } else {
-       next(createHttpError(400,{errors}))
+    if (errors.isEmpty()) {
+      const productsArray = await getProducts()
+      const newProduct = { ...req.body, _id: uniqid(), createAt: new Date(), imageUrl: `http://localhost:300/` }
+      productsArray.push(newProduct)
+      await writeProducts(productsArray)
+      res.status(201).send(`New product added with id - ${newProduct._id}`)
+    } else {
+      next(createHttpError(400, { errors }))
     }
-} catch (error) {
+  } catch (error) {
     next(error)
-}})
+  }
+})
 
 //************************ GET ALL PRODUCTS *********************************************
-productsRouter.get('/',async(req,res,next)=>{ try {
-
- const productsArray = await getProducts()
- res.status(200).send(productsArray)
-    
-} catch (error) {
-   next(error) 
-}})
+productsRouter.get("/", async (req, res, next) => {
+  try {
+    const productsArray = await getProducts()
+    res.status(200).send(productsArray)
+  } catch (error) {
+    next(error)
+  }
+})
 
 // get each product
-productsRouter.get('/:id',async(req,res,next)=>{ try {
-
+productsRouter.get("/:id", async (req, res, next) => {
+  try {
     const productsArray = await getProducts()
-    const specificProduct = productsArray.find(product => product._id === req.params.id)
+    const specificProduct = productsArray.find((product) => product._id === req.params.id)
     res.status(200).send(specificProduct)
-
-} catch (error) {
-   next(error) 
-}})
+  } catch (error) {
+    next(error)
+  }
+})
 
 // put edit
-productsRouter.put('/:id',async(req,res,next)=>{ try {
-
+productsRouter.put("/:id", async (req, res, next) => {
+  try {
     const productsArray = await getProducts()
-    const index = productsArray.findIndex(product => product._id === req.params.id)
+    const index = productsArray.findIndex((product) => product._id === req.params.id)
     const oldProduct = productsArray[index]
-    const updatedProduct = {...oldProduct, ...req.body, updatedAt: new Date()}
+    const updatedProduct = { ...oldProduct, ...req.body, updatedAt: new Date() }
     productsArray[index] = updatedProduct
-    await writeProducts(productsArray) 
+    await writeProducts(productsArray)
     res.send(updatedProduct)
-    
-} catch (error) {
-   next(error) 
-}})
+  } catch (error) {
+    next(error)
+  }
+})
 // delete
-productsRouter.delete('/:id',async(req,res,next)=>{
-     try 
-     {
+productsRouter.delete("/:id", async (req, res, next) => {
+  try {
     const productsArray = await getProducts()
-    const remainingProducts = productsArray.filter(product => product._id !== req.params.id)
+    const remainingProducts = productsArray.filter((product) => product._id !== req.params.id)
     await writeProducts(remainingProducts)
-    res.status(204).send("Product has been deleted")    //Status not found
-    
-} catch (error) {
-   next(error) 
-}})
+    res.status(204).send("Product has been deleted") //Status not found
+  } catch (error) {
+    next(error)
+  }
+})
 
 //****************************Review section*****************************
-//Post a Review 
-productsRouter.post('/:id/reviews', reviewValidator, async(req,res,next) => {
-    try {
-       const errors = validationResult(req)
+//Post a Review
+productsRouter.post("/:id/reviews", reviewValidator, async (req, res, next) => {
+  try {
+    const errors = validationResult(req)
 
-    if(errors.isEmpty()){
-        const productsArray = await getProducts() 
-        const productId = req.params.id
-        const index = productsArray.findIndex(product => product._id === productId);
-        const specificProduct = productsArray[index]
-        specificProduct.reviews = specificProduct.reviews || []
-        const newReview = {...req.body, productId: productId, _id: uniqid()}
-        const updatedProduct = {...specificProduct,reviews:[...specificProduct.reviews,newReview ]}
-        productsArray[index] = updatedProduct
-        await writeProducts(productsArray)
-        res.send({message:`New review added successfully- ${updatedProduct}`}) //Error
+    if (errors.isEmpty()) {
+      const productsArray = await getProducts()
+      const productId = req.params.id
+      const index = productsArray.findIndex((product) => product._id === productId)
+      const specificProduct = productsArray[index]
+      specificProduct.reviews = specificProduct.reviews || []
+      const newReview = { ...req.body, productId: productId, _id: uniqid() }
+      const updatedProduct = { ...specificProduct, reviews: [...specificProduct.reviews, newReview] }
+      productsArray[index] = updatedProduct
+      await writeProducts(productsArray)
+      res.send({ message: `New review added successfully- ${updatedProduct}` }) //Error
     } else {
-        next(createHttpError(400,{errors}))
-        }
-        
-    } catch (error) {
-       next(error) 
+      next(createHttpError(400, { errors }))
     }
+  } catch (error) {
+    next(error)
+  }
 })
 
 //GET All Reviews
 
-productsRouter.get('/:id/reviews', async (req,res,next) =>{
-    try {
-        const productsArray = await getProducts()
-        const singleProduct = productsArray.find(product => product._id === req.params.id)
-        // singleProduct.review = singleProduct.review || []
-        res.send(singleProduct.reviews)
-        
-    } catch (error) {
-        next(error)
-    }
-
+productsRouter.get("/:id/reviews", async (req, res, next) => {
+  try {
+    const productsArray = await getProducts()
+    const singleProduct = productsArray.find((product) => product._id === req.params.id)
+    // singleProduct.review = singleProduct.review || []
+    res.send(singleProduct.reviews)
+  } catch (error) {
+    next(error)
+  }
 })
 
 //GET single Review
 
-productsRouter.get('/:id/reviews/:reviewId', async (req,res,next) =>{
-    try {
-        const productsArray = await getProducts()
-        const singleProduct = productsArray.find(product => product._id === req.params.id)
-        const singleReview = singleProduct.reviews.find(review => review._id === req.params.reviewId)
-        res.send(singleReview)
-    } catch (error) {
-        next(error)
-    }
-
+productsRouter.get("/:id/reviews/:reviewId", async (req, res, next) => {
+  try {
+    const productsArray = await getProducts()
+    const singleProduct = productsArray.find((product) => product._id === req.params.id)
+    const singleReview = singleProduct.reviews.find((review) => review._id === req.params.reviewId)
+    res.send(singleReview)
+  } catch (error) {
+    next(error)
+  }
 })
 
 //EDIT single Review //PUT
 
-productsRouter.put('/:id/reviews/:reviewId', async (req,res,next) =>{
-    try {
-        const productsArray = await getProducts()
-        const productIndex = productsArray.findIndex(product => product._id === req.params.id)
-        const singleProduct = productsArray[productIndex]
-        const reviewIndex = singleProduct.reviews.findIndex(review => review._id === req.params.reviewId)
-        const singleReview = singleProduct.reviews[reviewIndex]
-        const updatedReview = {...singleReview,...req.body,updatedAt: new Date()}
-        singleProduct.reviews[reviewIndex] = updatedReview
-        productsArray[productIndex] = singleProduct
-        await writeProducts(productsArray)
-        res.send(updatedReview)
-    } catch (error) {
-        next(error)
-    }
+productsRouter.put("/:id/reviews/:reviewId", async (req, res, next) => {
+  try {
+    const productsArray = await getProducts()
+    const productIndex = productsArray.findIndex((product) => product._id === req.params.id)
+    const singleProduct = productsArray[productIndex]
+    const reviewIndex = singleProduct.reviews.findIndex((review) => review._id === req.params.reviewId)
+    const singleReview = singleProduct.reviews[reviewIndex]
+    const updatedReview = { ...singleReview, ...req.body, updatedAt: new Date() }
+    singleProduct.reviews[reviewIndex] = updatedReview
+    productsArray[productIndex] = singleProduct
+    await writeProducts(productsArray)
+    res.send(updatedReview)
+  } catch (error) {
+    next(error)
+  }
 })
 
 //DELETE single Review
 
-productsRouter.delete('/:id/reviews/:reviewId', async (req,res,next) =>{
-    try {
-        const productsArray = await getProducts()
-        const productIndex = productsArray.findIndex(product => product._id === req.params.id)
-        const singleProduct = productsArray[productIndex]
-        const remainingReviews = singleProduct.reviews.filter(review => review._id !== req.params.reviewId)
-        singleProduct.reviews= remainingReviews
+productsRouter.delete("/:id/reviews/:reviewId", async (req, res, next) => {
+  try {
+    const productsArray = await getProducts()
+    const productIndex = productsArray.findIndex((product) => product._id === req.params.id)
+    const singleProduct = productsArray[productIndex]
+    const remainingReviews = singleProduct.reviews.filter((review) => review._id !== req.params.reviewId)
+    singleProduct.reviews = remainingReviews
 
-        productsArray[productIndex] = singleProduct
-        await writeProducts(productsArray)
-        res.send()
-    } catch (error) {
-        next(error)
-    }
+    productsArray[productIndex] = singleProduct
+    await writeProducts(productsArray)
+    res.send()
+  } catch (error) {
+    next(error)
+  }
+})
+// *********************************file upload*********************************
+
+productsRouter.put("/:id/uploadProductImg", multer().single("image"), uploadFile, async (req, res, next) => {
+  try {
+    const productsArray = await getProducts()
+    const index = productsArray.findIndex((product) => product.id === req.params.id)
+
+    const singleProduct = productsArray[index]
+    const updatedBody = req.body
+    const updateProduct = { ...singleProduct, ...updatedBody, imageUrl: req.file, updatedAt: new Date() }
+    productsArray[index] = updateProduct
+
+    await writeProducts(productsArray)
+    res.send(updateProduct)
+  } catch (error) {}
 })
 
-
-
-
-
-
-
-
+productsRouter
 
 export default productsRouter
